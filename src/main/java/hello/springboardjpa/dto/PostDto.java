ef8ed4b8b375ec9ecd6c6123b0a706c3e840bd57
toughCircle <toughcircle@gmail.com>
2024-02-26 21:05:45 +0900

    first commit

--- src/main/java/hello/springboardjpa/dto/PostDto.java
+++ src/main/java/hello/springboardjpa/dto/PostDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
